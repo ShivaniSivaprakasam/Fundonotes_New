1541c36b9bca77d622b0c2de48375963829d1906
shivani <shivaniavanikkarasi@gmail.com>
2026-01-08 15:54:12 +0530

    Initial commit

---
+++ IdeaProjects/Exercise_2/src/Task21512/Age.java
@@ -0,0 +1,10 @@
+package Task21512;
+
+public class Age {
+    public static void main(String[] args) {
+        int DOB = 2000;
+        int currentYr = 2024;
+        int age = currentYr - DOB;
+        System.out.println("Harry\'s age in 2024 is: " +age);
+    }
+}
---
+++ IdeaProjects/Exercise_2/src/Task21512/ConvertToMiles.java
@@ -0,0 +1,13 @@
+package Task21512;
+
+import java.util.Scanner;
+public class ConvertToMiles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the distance in km: ");
+        double km = input.nextDouble();
+        double miles = km/1.6;
+        System.out.println("The total miles is " +miles + " mi miles for the given km " +km +" km.");
+    }
+}
+
